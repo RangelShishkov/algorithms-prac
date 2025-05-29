3c27387a717dea46fcc3f946fd72a5885580cd8b
Rangel Shishkov <rshishkov01@gmail.com>
2025-05-29 22:21:15 +0300

    commit

--- Easy/gradingStudents.js
+++ Easy/gradingStudents.js
@@ -4,33 +4,21 @@ function gradingStudents(grades) {
   // grades with mulitple of 3 and higher no rounding
   // grade rounding when multiple of <= 2
 
-  let secondNumber = "";
-  let numToStr = " ";
   let roundedGrades = [];
 
   for (let i = 0; i < grades.length; i++) {
-    numToStr = grades[i].toString();
-    secondNumber = numToStr.substring(1);
-
     if (grades[i] < 38) {
       roundedGrades.push(grades[i]);
-    } else if (
-      secondNumber == 1 ||
-      secondNumber == 2 ||
-      secondNumber == 5 ||
-      secondNumber == 6 ||
-      secondNumber == 7 ||
-      secondNumber == 0
-    ) {
+    } else if (grades[i] % 5 == 4) {
+      grades[i] += 1;
       roundedGrades.push(grades[i]);
-    } else if (secondNumber == 3 || secondNumber == 8) {
+    } else if (grades[i] % 5 == 3) {
       grades[i] += 2;
       roundedGrades.push(grades[i]);
-    } else if (secondNumber == 4 || secondNumber == 9) {
-      grades[i] += 1;
+    } else {
       roundedGrades.push(grades[i]);
     }
   }
-  return roundedGrades
+  return roundedGrades;
 }
 gradingStudents([73, 67, 38, 33]);
